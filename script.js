18ef78fc21fed816fb954ba5432609583c5859ff
Trueman Phamdo <trueman@phamdo.com>
2024-08-31 19:49:02 -0400

    Render gameboard to page

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 var gameboard = (function() {
-    let board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+    var _board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+
+    // cache dom
+    const $gameboardTemplate =
+        document.getElementById('gameboard-template').innerHTML;
+    const $gameboard = document.getElementById('gameboard');
+
+    _render();
 
     function makeMove(x, y, marker) {
+        console.log(_board);
         if (x<0 || y<0 || x>2 || y>2) {
-            console.error(`(x,y) must be between (0,0) and (2,2). Got (${x}, ${y}) instead`);
+            console.error(`(x,y) must be between (0,0) and (2,2).
+                Got (${x}, ${y}) instead`);
             return false;
         }
-        if (board[y][x] !== ' ') {
-            console.error(`Can not place on spot (${x},${y}). Spot already has a ${board[y][x]}`)
+        if (_board[y][x] !== ' ') {
+            console.error(`Can not place on spot (${x},${y}).
+                Spot already has a ${_board[y][x]}`)
             return false;
         }
-        board[y][x] = marker;
-        console.log(board);
+        _board[y][x] = marker;
+        _renderOneTile(x, y, marker);
         return true;
+    }
+
+    function _render() {
+        const rendered = Mustache.render($gameboardTemplate, { ..._board.flat() })
+        $gameboard.innerHTML = rendered;
+    }
+
+    function _renderOneTile(x, y, marker) {
+        console.log($gameboard.children);
+        $gameboard.children[x+3*y].textContent = marker;
     }
 
     function reset() {
         for (let y=0; y<3; ++y) {
             for (let x=0; x<3; ++x) {
-                board[y][x] = ' ';
+                _board[y][x] = ' ';
             }
         }
     }
@@ -26,26 +46,26 @@ var gameboard = (function() {
     function isWinner(marker) {
         // row
         for (let y=0; y<3; ++y) {
-            if (board[y][0] == marker &&
-                board[y][0] == board[y][1] &&
-                board[y][1] == board[y][2]) return true;
+            if (_board[y][0] == marker &&
+                _board[y][0] == _board[y][1] &&
+                _board[y][1] == _board[y][2]) return true;
         }
-        
+
         // col
         for (let x=0; x<3; ++x) {
-            if (board[0][x] == marker &&
-                board[0][x] == board[1][x] &&
-                board[1][x] == board[2][x]) return true;
+            if (_board[0][x] == marker &&
+                _board[0][x] == _board[1][x] &&
+                _board[1][x] == _board[2][x]) return true;
         }
 
         // diag
-        if (board[0][0] == marker &&
-            board[0][0] == board[1][1] &&
-            board[1][1] == board[2][2]) return true;
+        if (_board[0][0] == marker &&
+            _board[0][0] == _board[1][1] &&
+            _board[1][1] == _board[2][2]) return true;
 
-        if (board[0][2] == marker &&
-            board[0][2] == board[1][1] &&
-            board[1][1] == board[2][0]) return true;
+        if (_board[0][2] == marker &&
+            _board[0][2] == _board[1][1] &&
+            _board[1][1] == _board[2][0]) return true;
 
         return false;
     }
@@ -57,15 +77,40 @@ var game = (function() {
     let player1Turn = true;
     let gameOver = false;
     let movesMade = 0;
-   
-    function makeMove(x, y, marker) {
+
+    let player1 = {
+        marker: 'O',
+        makeMove: function(x,y) {
+            _makeMove(x,y,this.marker);
+        }
+    }
+
+    let player2 = {
+        marker: 'X',
+        makeMove: function(x,y) {
+            _makeMove(x,y,this.marker);
+        }
+    }
+
+    var tiles = document.querySelectorAll('.tile');
+    for(let i=0; i<tiles.length; i++){
+        console.log(tiles[i]);
+        tiles[i].onclick = function() {
+            console.log('onclick');
+            makeMove(i%3, Math.floor(i/3));
+            console.log('onclick2');
+        };
+    }
+
+    function makeMove(x,y) {
+        console.log(x,y);
+        if (player1Turn) player1.makeMove(x,y);
+        else player2.makeMove(x,y);
+    }
+
+    function _makeMove(x, y, marker) {
         if (gameOver) {
             console.error(`The game is over`)
-            return;
-        }
-
-        if (player1Turn && marker != 'O' || !player1Turn && marker != 'X') {
-            console.error(`It's Player${player1Turn? '1' : '2'}'s turn`)
             return;
         }
 
@@ -85,7 +130,9 @@ var game = (function() {
             }
 
             player1Turn = !player1Turn;
+            // console.log(player1Turn, movesMade, gameOver);
         }
+        // console.log(player1Turn, movesMade, gameOver);
     }
 
     function reset() {
@@ -102,22 +149,35 @@ var game = (function() {
     return { makeMove, reset };
 })();
 
-var player1 = {
-    marker: 'O',
-    makeMove: function(x,y) {
-        game.makeMove(x,y,this.marker);
+var pubSub = (function() {
+    var listeners = {};
+
+    function publish(eventName, data) {
+        if (listeners[eventName]) {
+            listeners[eventName].forEach(fn => {
+                fn(data);
+            });
+        }
     }
-}
 
-var player2 = {
-    marker: 'X',
-    makeMove: function(x,y) {
-        game.makeMove(x,y,this.marker);
+    function subscribe(eventName, fn) {
+        if (!listeners[eventName]) {
+            listeners[eventName] = [];
+        }
+        listeners[eventName].append(fn);
     }
-}
 
-const template = document.getElementById('template').innerHTML;
-const rendered = Mustache.render(template, { t0_0 : 'X', t1_0: 'O', t2_0: 'X' });
+    function unsubscribe(eventName, fn) {
+        var subscriptions = listeners[eventName]
+        if (subscriptions) {
+            for (let i=0; i<subscriptions.length; ++i) {
+                if (subscriptions[i] === fn) {
+                    subscriptions.splice(i,1);
+                    break;
+                }
+            }
+        }
+    }
 
-document.getElementById('target').innerHTML = rendered;
-
+    return { publish, subscribe, unsubscribe };
+});
